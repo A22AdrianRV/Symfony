a5035e8e6607263a65b9ab61539042b5931aaa85
A22AdrianRV <A22AdrianRV@iessanclemente.net>
2024-01-11 12:59:55 +0100

    añadido function

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
@@ -35,7 +36,7 @@ class VinylController extends AbstractController
         //     $genre = 'All Genres';
         // }
 
-        $genre = $slug ? str_replace('-', ' ', $slug)->title(true) : null;
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
         return $this->render('vinyl/browse.html.twig',[
             'genre' => $genre
         ]);
